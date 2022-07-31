128465e72fc235d275f6f49c2bdfad89e63e0b90
Orion Aon <orion.aon@gmail.com>
2022-07-31 13:54:47 -0600

    edit files

---
+++ public/main.js
@@ -0,0 +1,2 @@
+// This is where the code for contacting the server and passing information back and forth goes.
+// Info can trvael back and forth using fetches.
--- server.js
+++ server.js
@@ -1,21 +1,36 @@
+// set up variables
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
+// variables for mongo connection
 let db,
     dbConnectionString = process.env.DB_STRING,
+    // db name should change to reflect the db for the app being built
     dbName = 'sample_mflix',
     collection 
 
+// set up mongo connection
 MongoClient.connect(dbConnectionString)
     .then(client => {
         console.log('Connected to Database')
         db = client.db(dbName)
+        // 'comments' should change to reflect the collection for the app being built
         collection = db.collection('comments')
     })
 
+// middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors)
+
+
+
+// set up port connecton
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on the port`)
 })
